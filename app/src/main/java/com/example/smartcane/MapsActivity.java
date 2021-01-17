5f8cd84131249c2f0e0dffe8c38852da8847553d
Wiggling Paws <44749612+wigglingpaws@users.noreply.github.com>
2021-01-17 12:30:17 +0800

    Update MapsActivity.java

--- app/src/main/java/com/example/smartcane/MapsActivity.java
+++ app/src/main/java/com/example/smartcane/MapsActivity.java
@@ -30,21 +30,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+   
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
